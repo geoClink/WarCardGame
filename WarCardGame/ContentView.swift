17b74555be0323b4860d4befe4817ce2832a03d7
George C <1lclink2@att.net>
2025-11-04 21:58:54 -0500

    Initial Commit

---
+++ WarCardGame/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  WarCardGame
+//
+//  Created by George Clinkscales on 11/4/25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
